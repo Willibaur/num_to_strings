1597b06c39d828bf107df711f24eb706d2e3bbc2
William Bautista <williambautista@gmail.com>
2015-11-21 20:02:25 +0000

    Final rspec version

--- spec/num_to_strings_spec.rb
+++ spec/num_to_strings_spec.rb
@@ -23,10 +23,26 @@ describe 'num_to_strings' do
         0 =>'zero', 1=>'one', 2=>'two', 3=>'three', 4=>'four', 5=>'five', 6=>'six', 7=>'seven', 8=>'eight', 9=>'nine', 10=>'ten', 11=>'eleven', 12=>'twelve', 13=>'thirteen', 14=>'fourteen', 15=>'fifteen', 16=>'sixteen', 17=>'seventeen', 18=>'eighteen', 19=>'nineteen'
      }
 
-    #  qty_20s_to_90s = {
-    #    2=>'twenty', 3=>'thirty', 4=>'forty', 5=>'fifty', 6=>'sixty', 7=>'seventy', 8=>'eighty', 9=>'ninety'
+    qty_20s_to_90s = {
+       2=>'twenty', 3=>'thirty', 4=>'forty', 5=>'fifty', 6=>'sixty', 7=>'seventy', 8=>'eighty', 9=>'ninety'
+     }
 
-    qty_1s_to19s.each {|key, value| expect(num_to_strings(key.to_s)).to eq value}
+    # qty_1s_to19s.each {|key, value| expect(num_to_strings(key.to_s)).to eq value}
+
+    99.times do |number|
+      first  = number.div(10)
+      second = number % 10
+
+      if number < 20
+        expect(num_to_strings(number.to_s)).to eq qty_1s_to19s[number]
+      elsif number >= 20 && second != 0
+        expect(num_to_strings(number.to_s)).to eq qty_20s_to_90s[first] +" "+ qty_1s_to19s[second]
+      else number >= 20 && second == 0
+        expect(num_to_strings(number.to_s)).to eq qty_20s_to_90s[first]
+      end
+
+    end
+
   end
 
 end
